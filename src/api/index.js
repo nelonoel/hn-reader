afc60037a415952bb2aa36757d034fd98e40c940
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-06-12 14:01:41 +0800

    Add API interface

---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+export function getNewStories() {
+  const apiURL = "https://hacker-news.firebaseio.com/v0/newstories.json"
+  return fetch(apiURL).then(res => res.json())
+}
+
+export function getItem(id) {
+  const apiURL = `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+  const storyURL = id => `https://news.ycombinator.com/item?id=${id}.json`
+
+  return fetch(apiURL)
+    .then(res => res.json())
+    .then(story => {
+      return {
+        title: story.title,
+        author: story.by,
+        url: "url" in story ? story.url : storyURL(story.id)
+      }
+    })
+}
