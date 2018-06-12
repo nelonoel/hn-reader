3124f36ca0e14dd540704fd41951fe8006b52a27
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-06-12 18:39:58 +0800

    Remove linked stylesheet on prod

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -260,6 +260,10 @@ module.exports = {
         minifyURLs: true,
       },
 		}),
+    // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
+    new ExtractTextPlugin({
+      filename: cssFilename,
+    }),
 		// Move styles within <styles> tag
 		new StyleExtHtmlWebpackPlugin(),
     // Makes some environment variables available to the JS code, for example:
@@ -287,10 +291,6 @@ module.exports = {
         ascii_only: true,
       },
       sourceMap: shouldUseSourceMap,
-    }),
-    // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
-    new ExtractTextPlugin({
-      filename: cssFilename,
     }),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
