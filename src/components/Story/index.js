561d571cdf2fa2b282c5290b70a5eec665394a20
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-06-12 14:40:02 +0800

    Add Story component

---
+++ src/components/Story/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react"
+import "./index.css"
+import { getItem } from "../../api"
+
+export default class Story extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "",
+      author: "",
+      url: ""
+    }
+  }
+
+  componentWillMount() {
+    const { id } = this.props
+    getItem(id).then(story => {
+      this.setState(story)
+    })
+  }
+
+  render() {
+    const { title, author, url } = this.state
+    const story = (
+      <li className="story">
+        <a href={url} target="blank">
+          {title}
+        </a>{" "}
+        <br />
+        {author}
+      </li>
+    )
+
+    // Rendering an empty div maintains the order
+    return title.length > 0 ? story : <div />
+  }
+}
