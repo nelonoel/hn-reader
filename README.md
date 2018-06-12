a217c903a76f44f8ec3c1f6d34f619a055f3d442
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2018-06-12 14:40:54 +0800

    Fetch story ids, add infinite-scrolling

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Create an offline-first, blazing fast HackerNews reader
 
 ### Todo
 
-- [ ] Fetch new stories
-- [ ] Load each story
-- [ ] Infinite scrolling
+- [x] Fetch new stories
+- [x] Load each story
+- [x] Infinite scrolling
 - [ ] Manage cache for offline access
 - [ ] Optimize build and performance
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react"
+import Story from "../Story"
+import "./index.css"
+import { getNewStories } from "../../api"
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      page: 1,
+      ids: []
+    }
+  }
+
+  componentWillMount() {
+    const _this = this
+    if (typeof window !== "undefined") {
+      window.onscroll = () => {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+          _this.setState({
+            page: _this.state.page + 1
+          })
+        }
+      }
+    }
+
+    getNewStories().then(ids => {
+      this.setState({ ids })
+    })
+  }
+
   render() {
-    return <div className="app" />
+    const { ids, page } = this.state
+    const { items } = this.props
+
+    return (
+      <ul className="app">
+        <h1>Quick HN Reader</h1>
+        {ids.slice(0, page * items).map(id => <Story key={id} id={id} />)}
+      </ul>
+    )
   }
 }
-
-export default App
